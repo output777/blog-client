6cb372faa6ef04bcb35aa03810910e50b534d72b
oh <outputor777@gmail.com>
2024-05-06 21:42:13 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import {NextResponse} from 'next/server';
-import type {NextRequest} from 'next/server';
-import {auth} from './auth';
+// import {NextResponse} from 'next/server';
+// import type {NextRequest} from 'next/server';
+// import {auth} from './auth';
 
-export async function middleware(request: NextRequest) {
-  const session = await auth();
-  console.log('session1', session, !session);
-  if (!session) {
-    console.log('session2', session);
-    console.log('request.url', request.url);
-    // return NextResponse.rewrite(new URL('/signin', request.url));
-  }
-}
+// export async function middleware(request: NextRequest) {
+//   const session = await auth();
+//   console.log('session1', session, !session);
+//   if (!session) {
+//     console.log('session2', session);
+//     console.log('request.url', request.url);
+//     // return NextResponse.rewrite(new URL('/signin', request.url));
+//   }
+// }
 
-export const config = {
-  matcher: ['/blog'],
-};
+// export const config = {
+//   matcher: ['/blog'],
+// };
