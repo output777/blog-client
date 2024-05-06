9bf41a0da6ccd134ec5053397ab1d47a33f77d3b
oh <outputor777@gmail.com>
2024-05-06 22:04:51 +0900

    test

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,10 +1,15 @@
 import {ReactNode} from 'react';
 import AtferMenu from '@/_components/AfterMenu';
 import {auth} from '@/auth';
+import {redirect} from 'next/navigation';
 
 type Props = {children: ReactNode};
 export default async function Layout({children}: Props) {
   const session = await auth();
+
+  if (!session) {
+    redirect('/signin');
+  }
 
   return (
     <div>
