e7633c111c71a6fc24d5b62c98399b7e969706f0
oh <outputor777@gmail.com>
2024-05-06 21:19:39 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,14 @@
 import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
 import {auth} from './auth';
 
-export async function middleware() {
+export async function middleware(request: NextRequest) {
   const session = await auth();
   if (!session) {
-    // return NextResponse.redirect('/signin');
+    return NextResponse.rewrite(new URL('/signin', request.url));
   }
 }
 
 export const config = {
-  // matcher: ['/blog'],
+  matcher: ['/blog'],
 };
