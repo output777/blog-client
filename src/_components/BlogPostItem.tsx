49351d5beceb8ae838ea453e7690e8a960c8b536
oh <outputor777@gmail.com>
2024-05-16 11:41:39 +0900

    delete post

--- src/_components/BlogPostItem.tsx
+++ src/_components/BlogPostItem.tsx
@@ -1,20 +1,38 @@
 'use client';
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import styles from './styles/blogPosts.module.css';
 import {useUrlParamsNicknameStore} from '@/app/_store/urlParamsNicknameStore';
 import {PostProps} from '@/app/blog/[nickname]/page';
-import {useQuery} from '@tanstack/react-query';
-import {useParams} from 'next/navigation';
+import {useMutation, useQuery} from '@tanstack/react-query';
+import {useParams, useRouter} from 'next/navigation';
 import {HiOutlineDotsVertical} from 'react-icons/hi';
 import ContentComponent from './ContentComponent';
 import {regFullTime} from '@/app/_lib/time';
+import {SlPencil} from 'react-icons/sl';
+import {BsTrash3} from 'react-icons/bs';
 
 export default function BlogPostItem() {
+  const router = useRouter();
   const params = useParams();
   const {nickname} = useUrlParamsNicknameStore();
   const postId = params.postid;
+  const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
+
+  const deleteMutation = useMutation({
+    mutationFn: async (postId: string) => {
+      const url = new URL('/api/deletepost', window.location.origin);
+      url.searchParams.append('postId', postId);
+
+      const response = await fetch(url.toString(), {method: 'DELETE'});
+      console.log('response', response);
+      return await response.json();
+    },
+    onSuccess: () => {
+      router.back();
+    },
+  });
 
   const {data: postData, isFetching: isFetchingPostData} = useQuery<PostProps>({
     queryKey: ['posts', postId],
@@ -28,6 +46,36 @@ export default function BlogPostItem() {
     enabled: !!postId,
   });
 
+  const infoButtonhandler = (postId: number) => {
+    if (selectedPostId === postId) {
+      setSelectedPostId(null);
+    } else {
+      setSelectedPostId(postId);
+    }
+  };
+
+  const handleClickOutside = (e: Event) => {
+    const target = e.target as HTMLElement;
+    if (target.id === 'delete_button') {
+      return;
+    }
+    if (target.id !== 'info_button') {
+      setSelectedPostId(null);
+    }
+  };
+
+  async function deletHandler() {
+    deleteMutation.mutate(postId as string);
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={styles.post_item_wrap}>
       <div className={styles.posts_content} key={postData?.post_id}>
@@ -40,7 +88,33 @@ export default function BlogPostItem() {
               <span>{regFullTime(postData?.reg_tm as string)}</span>
             </div>
             <div className={styles.posts_content_header_info_button_wrap}>
-              <HiOutlineDotsVertical className={styles.posts_content_header_info_button} />
+              <HiOutlineDotsVertical
+                className={styles.posts_content_header_info_button}
+                onClick={() => infoButtonhandler(postData?.post_id as number)}
+                id="info_button"
+              />
+              <div
+                className={`${styles.posts_content_header_info_button_menu} ${
+                  selectedPostId !== postData?.post_id ? styles.none : ''
+                }`}
+              >
+                <Link
+                  href={`/blog/${nickname}/update/${postData?.post_id}`}
+                  className={styles.posts_content_header_info_button_modify}
+                  id="modify_button"
+                >
+                  <span>수정하기</span>
+                  <SlPencil />
+                </Link>
+                <div
+                  className={styles.posts_content_header_info_button_delete}
+                  id="delete_button"
+                  onClick={deletHandler}
+                >
+                  <span>삭제하기</span>
+                  <BsTrash3 />
+                </div>
+              </div>
             </div>
           </div>
           <div className={styles.posts_content_header_border_bottom}></div>
