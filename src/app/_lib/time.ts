256d8567f8ec4eedb8ee22806fa4d92dbee89573
young il Oh <youngiloh@youngui-MacBookPro.local>
2024-05-24 13:14:53 +0900

    infinite fix

--- src/app/_lib/time.ts
+++ src/app/_lib/time.ts
@@ -1,12 +1,6 @@
-function toKST(date: Date) {
-  const KST_OFFSET = 9 * 60 * 60 * 1000; // 한국 시간은 UTC+9
-  return new Date(date.getTime() + KST_OFFSET);
-}
-
 export function timeAgo(inputTime: string) {
-  const now = toKST(new Date()); // 현재 시간 KST로 변환
-  const time = toKST(new Date(inputTime)); // 입력 시간 UTC로 해석 후 KST로 변환
-
+  const now = new Date(); 
+  const time = new Date(inputTime);
   const diffInSeconds = Math.floor((now.getTime() - time.getTime()) / 1000);
   const diffInMinutes = Math.floor(diffInSeconds / 60);
   const diffInHours = Math.floor(diffInMinutes / 60);
@@ -24,7 +18,7 @@ export function timeAgo(inputTime: string) {
 }
 
 export function regTime(inputTime: string) {
-  const time = toKST(new Date(inputTime)); // 입력 시간을 KST로 변환
+  const time = new Date(inputTime);
   const year = time.getFullYear();
   const month = String(time.getMonth() + 1).padStart(2, '0');
   const date = String(time.getDate()).padStart(2, '0');
@@ -32,7 +26,7 @@ export function regTime(inputTime: string) {
 }
 
 export function regFullTime(inputTime: string) {
-  const time = toKST(new Date(inputTime)); // 입력 시간을 KST로 변환
+  const time = new Date(inputTime);
   const year = time.getFullYear();
   const month = String(time.getMonth() + 1).padStart(2, '0');
   const date = String(time.getDate()).padStart(2, '0');
